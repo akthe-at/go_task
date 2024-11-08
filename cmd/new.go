1cafdf559e6db5ce421ffa2ae7f3142b643c3b6a
Adam K. <arkelly2@medicine.wisc.edu>
2024-11-07 22:11:24 -0600

    add cli cmds for adding new notes, projects/areas

--- cmd/new.go
+++ cmd/new.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/akthe-at/go_task/data"
 	"github.com/akthe-at/go_task/db"
@@ -65,9 +66,91 @@ and usage of using your command. For example:
 	},
 }
 
+// newProjectCmd represents the new command
+var newProjectCmd = &cobra.Command{
+	Use:   "project",
+	Short: "Creates a new project with a form",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		form := &formInput.NewAreaForm{}
+
+		err := form.NewAreaForm()
+		if err != nil {
+			log.Fatalf("Error creating form: %v", err)
+		}
+
+		if form.Submit {
+
+			newTask := data.Area{
+				Title:    form.AreaTitle,
+				Status:   form.Status,
+				Archived: form.Archived,
+			}
+
+			conn, err := db.ConnectDB()
+			if err != nil {
+				log.Fatalf("Error connecting to database: %v", err)
+			}
+			defer conn.Close()
+			theArea := data.AreaTable{
+				Area: newTask,
+			}
+			err = theArea.Create(conn)
+			if err != nil {
+				log.Fatalf("Error creating task: %v", err)
+			}
+		}
+	},
+}
+
+// newTaskNoteCmd represents the new command
+var newTaskNoteCmd = &cobra.Command{
+	Use:   "note",
+	Short: "Add a note to a specific task",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) < 3 {
+			log.Fatalf("Usage: note <task_id> <note_title> <note_path>")
+		}
+
+		taskID, err := strconv.Atoi(args[0])
+		if err != nil {
+			log.Fatalf("Invalid task ID: %v", err)
+		}
+
+		noteTitle := args[1]
+		notePath := args[2]
+
+		conn, err := db.ConnectDB()
+		if err != nil {
+			log.Fatalf("Error connecting to database: %v", err)
+		}
+		defer conn.Close()
+
+		note := data.Note{
+			Title: noteTitle,
+			Path:  notePath,
+		}
+
+		taskTable := data.TaskTable{}
+		err = data.AddNoteToEntity(&taskTable, conn, taskID, note)
+		if err != nil {
+			log.Fatalf("Error adding note to task: %v", err)
+		}
+
+		fmt.Println("Note added to task successfully")
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(newCmd)
 	newCmd.AddCommand(newTaskCmd)
+	newCmd.AddCommand(newProjectCmd)
+	newCmd.AddCommand(newTaskNoteCmd)
 
 	// Here you will define your flags and configuration settings.
 
