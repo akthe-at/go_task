e1f2865dabac2681dea01a2644bfc496d88b24f0
Adam K. <arkelly2@medicine.wisc.edu>
2024-12-22 04:34:13 -0600

    improve new note form, pre query options instead of waiting

--- tui/formInput/NewNote.go
+++ tui/formInput/NewNote.go
@@ -1,9 +1,15 @@
 package formInput
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/akthe-at/go_task/data"
+	"github.com/akthe-at/go_task/db"
+	"github.com/akthe-at/go_task/sqlc"
 	"github.com/akthe-at/go_task/tui"
 	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
 )
 
 type NewNoteForm struct {
@@ -11,11 +17,14 @@ type NewNoteForm struct {
 	Title    string
 	Path     string
 	Type     data.NoteType
+	ParentID int
 	Submit   bool
 }
 
 func (n *NewNoteForm) NewNoteForm() error {
 	tui.ClearTerminalScreen()
+	taskOptions := fetchNoteParent(data.TaskNoteType)
+	areaOptions := fetchNoteParent(data.AreaNoteType)
 
 	noteGroups := []*huh.Group{
 		huh.NewGroup(
@@ -28,10 +37,31 @@ func (n *NewNoteForm) NewNoteForm() error {
 				Prompt(">").
 				Value(&n.Path),
 			huh.NewSelect[data.NoteType]().
-				Options(huh.NewOptions(data.TaskNoteType, data.AreaNoteType)...).
 				Title("What type of note is this?").
 				Description("Choose a type").
+				Options(
+					huh.NewOption("Task Note", data.TaskNoteType),
+					huh.NewOption("Area Note", data.AreaNoteType),
+				).
 				Value(&n.Type),
+		),
+		huh.NewGroup(
+			huh.NewSelect[int]().
+				Value(&n.ParentID).
+				Title("Which Task Did You Want to Assign This Note To?").
+				Options(taskOptions...),
+		).WithHideFunc(func() bool {
+			return n.Type != data.TaskNoteType
+		}),
+		huh.NewGroup(
+			huh.NewSelect[int]().
+				Value(&n.ParentID).
+				Title("Which Area Did You Want to Assign This Note To?").
+				Options(areaOptions...),
+		).WithHideFunc(func() bool {
+			return n.Type != data.AreaNoteType
+		}),
+		huh.NewGroup(
 			huh.NewConfirm().
 				Title("Are you ready to save your note?").
 				Affirmative("Yes").
@@ -42,4 +72,38 @@ func (n *NewNoteForm) NewNoteForm() error {
 	n.NoteForm = huh.NewForm(noteGroups...)
 
 	return n.NoteForm.Run()
+}
+
+func fetchNoteParent(selection data.NoteType) []huh.Option[int] {
+	var options []huh.Option[int]
+	ctx := context.Background()
+
+	conn, err := db.ConnectDB()
+	if err != nil {
+		log.Errorf("There was an error connecting to the database: %v", err)
+	}
+
+	queries := sqlc.New(conn)
+	defer conn.Close()
+
+	switch selection {
+	case data.TaskNoteType:
+		tasks, err := queries.ReadAllTasks(ctx)
+		if err != nil {
+			return nil
+		}
+		for _, task := range tasks {
+			options = append(options, huh.NewOption(fmt.Sprintf("%s - %d", task.Title, task.ID), int(task.ID)))
+		}
+	case data.AreaNoteType:
+		notes, err := queries.ReadAllNotes(ctx)
+		if err != nil {
+			return nil
+		}
+		for _, note := range notes {
+			options = append(options, huh.NewOption(note.Title, int(note.ID)))
+		}
+	}
+
+	return options
 }
